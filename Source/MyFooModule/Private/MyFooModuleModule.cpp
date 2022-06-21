dcdb92d4505ec17a4831cf1503a4863f559a1c20
Gargowle <84672530+Gargowle@users.noreply.github.com>
2022-06-21 21:33:26 +0200

    Add explanation on used macro

--- Source/MyFooModule/Private/MyFooModuleModule.cpp
+++ Source/MyFooModule/Private/MyFooModuleModule.cpp
@@ -1,4 +1,5 @@
 #include "Modules/ModuleManager.h"
 
 // Default implementation of loader, resulting in default loading and unloading behavior
+// This is only here by convention - it could be in any cpp file of this module
 IMPLEMENT_MODULE(FDefaultModuleImpl, MyFooModule);
