35483d6ab639f0fc7f3b4305536e87ace963a3fd
Gargowle <84672530+Gargowle@users.noreply.github.com>
2022-09-01 12:24:23 +0200

    Module for TextAsset editor support (still empty)

---
+++ Plugins/TextAsset/Source/TextAssetEditor/Private/TextAssetEditor.cpp
@@ -0,0 +1,15 @@
+﻿#include "Modules/ModuleManager.h"
+
+#define LOCTEXT_NAMESPACE "FTextAssetEditorModule"
+
+
+class FTextAssetEditorModule : public IModuleInterface
+{
+public:
+	virtual void StartupModule() override {};
+	virtual void ShutdownModule() override {};
+};
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FTextAssetEditorModule, TextAssetEditor)
