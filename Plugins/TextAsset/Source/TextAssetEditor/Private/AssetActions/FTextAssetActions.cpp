e1a1330b8fdd584abfd81979c7493ed8a1ba9201
Gargowle <84672530+Gargowle@users.noreply.github.com>
2022-09-02 12:12:31 +0200

    Add action on TextAssets to RMB menu in content browser
    Action added by implementing the GetActions method of the TextAssetActions class

--- Plugins/TextAsset/Source/TextAssetEditor/Private/AssetActions/FTextAssetActions.cpp
+++ Plugins/TextAsset/Source/TextAssetEditor/Private/AssetActions/FTextAssetActions.cpp
@@ -1,13 +1,14 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "FTextAssetActions.h"
 
 #include "TextAsset.h"
 
+#define LOCTEXT_NAMESPACE "AssetTypeActions"
+
 FText FTextAssetActions::GetName() const
 {
-	return NSLOCTEXT("AssetTypeActions", "AssetTypeActions_TextAsset", "Text Asset");
+	return LOCTEXT("AssetTypeActions_TextAsset", "Text Asset");
 }
 
 UClass* FTextAssetActions::GetSupportedClass() const
@@ -24,3 +25,47 @@ uint32 FTextAssetActions::GetCategories()
 {
 	return EAssetTypeCategories::Misc;
 }
+
+void FTextAssetActions::GetActions(const TArray<UObject*>& InObjects, FMenuBuilder& MenuBuilder)
+{
+	FAssetTypeActions_Base::GetActions(InObjects, MenuBuilder);
+
+	// trick to only operate on TextAssets because also other objects could be selected that are no TextAssets
+	// non-TextAssets will return false in IsValid check down below
+	TArray<TWeakObjectPtr<UTextAsset>> TextAssets = GetTypedWeakObjectPtrs<UTextAsset>(InObjects);
+
+	// Add the action to reverse the text stored in the selected TextAssets
+	MenuBuilder.AddMenuEntry(
+		LOCTEXT("TextAsset_ReverseText", "Reverse Text"),
+		LOCTEXT("TextAsset_ReverseTextToolTip", "Reverse the text stored in teh selected text asset(s)."),
+		FSlateIcon(),
+		FUIAction(
+			FExecuteAction::CreateLambda([=]
+			{
+				for (auto& TextAsset : TextAssets)
+				{
+					if(TextAsset.IsValid() && !TextAsset->Text.IsEmpty())
+					{
+						TextAsset->Text = FText::FromString(TextAsset->Text.ToString().Reverse());
+						TextAsset->PostEditChange();
+						TextAsset->MarkPackageDirty();
+					}
+				}
+			}),
+			FCanExecuteAction::CreateLambda([=]
+			{
+				// action can be executed once at least one non-empty TextAsset in selection
+				for (auto& TextAsset : TextAssets)
+				{
+					if(TextAsset.IsValid() && !TextAsset->Text.IsEmpty())
+					{
+						return true;
+					}
+				}
+				return false;
+			})
+		)
+	);
+}
+
+#undef LOCTEXT_NAMESPACE
--- Plugins/TextAsset/Source/TextAssetEditor/Private/AssetActions/FTextAssetActions.h
+++ Plugins/TextAsset/Source/TextAssetEditor/Private/AssetActions/FTextAssetActions.h
@@ -21,4 +21,15 @@ public:
 
 	/* method that needs to be overwritten for the dialog to add an instance of the class to appear in the editor's add dialog */
 	virtual uint32 GetCategories() override;
+
+	/* for better clarity, override the CanFilter and HasActions methods to return true explicitly */
+	virtual bool CanFilter() override { return true; }
+	virtual bool HasActions(const TArray<UObject*>& InObjects) const override { return true; }
+
+	/**
+	 * Add actions that can be performed from the right-click menu when right-clicking on TextAssets selected in the content browser
+	 */
+	virtual void GetActions(const TArray<UObject*>& InObjects, FMenuBuilder& MenuBuilder) override;
+	//implement after editor has been implemented//virtual void OpenAssetEditor(const TArray<UObject*>& InObjects, TSharedPtr<IToolkitHost> EditWithinLevelEditor) override; 
+	
 };
