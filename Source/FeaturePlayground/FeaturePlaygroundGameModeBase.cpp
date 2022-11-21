1b9299b004711cb105793e15206e579b2d14ed77
Gargowle <84672530+Gargowle@users.noreply.github.com>
2022-11-21 21:17:43 +0100

    Execute dummy code on a Task Graph worker thread using AsyncTask

--- Source/FeaturePlayground/FeaturePlaygroundGameModeBase.cpp
+++ Source/FeaturePlayground/FeaturePlaygroundGameModeBase.cpp
@@ -3,3 +3,18 @@
 
 #include "FeaturePlaygroundGameModeBase.h"
 
+#include "HAL/ThreadManager.h"
+
+void AFeaturePlaygroundGameModeBase::StartPlay()
+{
+	Super::StartPlay();
+
+	FString ThreadName = FThreadManager::Get().GetThreadName(FPlatformTLS::GetCurrentThreadId());
+	UE_LOG(LogTemp, Warning, TEXT("Start Play; Thread Name: %s"), *ThreadName);
+	
+	AsyncTask(ENamedThreads::AnyNormalThreadNormalTask,[]()
+	{
+		FString ThreadName = FThreadManager::Get().GetThreadName(FPlatformTLS::GetCurrentThreadId());
+		UE_LOG(LogTemp, Warning, TEXT("Somewhere in the TaskGraph; Thread Name: %s"), *ThreadName);
+	});
+}
--- Source/FeaturePlayground/FeaturePlaygroundGameModeBase.h
+++ Source/FeaturePlayground/FeaturePlaygroundGameModeBase.h
@@ -13,5 +13,8 @@ UCLASS()
 class FEATUREPLAYGROUND_API AFeaturePlaygroundGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
+
+public:
+	virtual void StartPlay() override;
 	
 };
